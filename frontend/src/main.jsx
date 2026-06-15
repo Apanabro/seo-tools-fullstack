2c3b0a7a7c031fe8c8859e5069748483ebab2d74
Apanabro <Apanabro@users.noreply.github.com>
2026-06-15 23:08:21 +0530

    Fix nested router conflict (main.jsx had BrowserRouter, App.jsx had HashRouter)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { ThemeProvider } from './context/ThemeContext'
 import './styles/global.css'
@@ -10,9 +9,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <AuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <App />
       </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
